0020e29452a32b310bc50032fba9d5c63f68e5a8
Huy Mai <quochuy0703@gmail.com>
2024-03-04 16:26:39 +0700

    1. maintain UserRepository

--- repository/UserRepository.cpp
+++ repository/UserRepository.cpp
@@ -8,10 +8,11 @@ namespace app_repositories::user_repository
 
     drogon::Task<std::vector<drogon_model::test::UserLogin>> getAll()
     {
-        auto db = drogon::app().getDbClient();
+
         std::vector<UserModel> users;
         try
         {
+            auto db = drogon::app().getDbClient();
             drogon::orm::CoroMapper<UserModel> usr(db);
             users = co_await usr.findAll();
         }
@@ -25,11 +26,12 @@ namespace app_repositories::user_repository
 
     drogon::Task<drogon_model::test::UserLogin> getById(int id)
     {
-        auto db = drogon::app().getDbClient();
+
         UserModel user;
 
         try
         {
+            auto db = drogon::app().getDbClient();
             drogon::orm::CoroMapper<UserModel> usr(db);
             user = co_await usr.findOne(drogon::orm::Criteria(UserModel::Cols::_id, id));
         }
@@ -42,10 +44,11 @@ namespace app_repositories::user_repository
     }
     drogon::Task<drogon_model::test::UserLogin> create(drogon_model::test::UserLogin user)
     {
-        auto db = drogon::app().getDbClient();
+
         UserModel userReturn;
         try
         {
+            auto db = drogon::app().getDbClient();
             drogon::orm::CoroMapper<UserModel> usr(db);
             userReturn = co_await usr.insert(user);
         }
@@ -59,12 +62,13 @@ namespace app_repositories::user_repository
 
     drogon::Task<bool> update(drogon_model::test::UserLogin user)
     {
-        auto db = drogon::app().getDbClient();
+
         int idPrimaryKey = *user.getId();
         UserModel userExist;
         bool result;
         try
         {
+            auto db = drogon::app().getDbClient();
             drogon::orm::CoroMapper<UserModel> usr(db);
             userExist = co_await usr.findByPrimaryKey(idPrimaryKey);
             result = co_await usr.update(user);
@@ -79,10 +83,11 @@ namespace app_repositories::user_repository
 
     drogon::Task<drogon_model::test::UserLogin> remove(int id)
     {
-        auto db = drogon::app().getDbClient();
+
         bool result;
         try
         {
+            auto db = drogon::app().getDbClient();
             drogon::orm::CoroMapper<UserModel> usr(db);
             result = co_await usr.deleteBy(drogon::orm::Criteria(UserModel::Cols::_id, id));
         }
