46b56a15b7b90694cd513713fb213e72e9dad273
quochuy0703 <quochuy0703@gmail.com>
2024-08-19 20:02:22 +0700

    1. modify config

--- main.cc
+++ main.cc
@@ -1,9 +1,11 @@
 #include <drogon/drogon.h>
 #include <iostream>
 #include <chrono>
+#include "./aop/Application.h"
 #include "./utils/ExecuteAwaiter.hpp"
 #include "./utils/EmailHelper.hpp"
 #include "./utils/Utils.hpp"
+
 
 using namespace drogon;
 
@@ -15,6 +17,7 @@ int main()
 	// drogon::app().addListener("0.0.0.0", 5555);
 	// Load config file
 	drogon::app().loadConfigFile("../config.json");
+	App::Application();
 	// drogon::app().loadConfigFile("../config.yaml");
 	// Run HTTP framework,the method will block in the internal event loop
 	drogon::app().registerHandler("/test_request_coro/{1}", [](drogon::HttpRequestPtr req, std::function<void(const drogon::HttpResponsePtr &)> callback, std::string userId) -> drogon::AsyncTask
