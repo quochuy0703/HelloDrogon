317417542d516785ca17c7ba81c2d499505d9ed9
quochuy0703 <quochuy0703@gmail.com>
2024-09-01 10:23:14 +0700

    1. add test

--- test/test_main.cc
+++ test/test_main.cc
@@ -2,12 +2,23 @@
 #include <drogon/drogon_test.h>
 #include <drogon/drogon.h>
 
+int sum_all(int n)
+{
+    int result = 1;
+    for (int i = 2; i < n; i++)
+        result += i;
+    return result;
+}
+
 DROGON_TEST(BasicTest)
 {
     // Add your tests here
+    CHECK(sum_all(1) == 1);
+    CHECK(sum_all(2) == 3);
+    CHECK(sum_all(3) == 6);
 }
 
-int main(int argc, char** argv) 
+int main(int argc, char **argv)
 {
     using namespace drogon;
 
@@ -15,18 +26,19 @@ int main(int argc, char** argv)
     std::future<void> f1 = p1.get_future();
 
     // Start the main loop on another thread
-    std::thread thr([&]() {
+    std::thread thr([&]()
+                    {
         // Queues the promise to be fulfilled after starting the loop
         app().getLoop()->queueInLoop([&p1]() { p1.set_value(); });
-        app().run();
-    });
+        app().run(); });
 
     // The future is only satisfied after the event loop started
     f1.get();
     int status = test::run(argc, argv);
 
     // Ask the event loop to shutdown and wait
-    app().getLoop()->queueInLoop([]() { app().quit(); });
+    app().getLoop()->queueInLoop([]()
+                                 { app().quit(); });
     thr.join();
     return status;
 }
