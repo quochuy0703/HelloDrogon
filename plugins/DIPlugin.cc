df4a191fa620047c8da50a47756fbb6c5a96a709
quochuy0703 <quochuy0703@gmail.com>
2025-01-02 19:46:16 +0700

    1. update DIPlugin

--- plugins/DIPlugin.cc
+++ plugins/DIPlugin.cc
@@ -9,13 +9,32 @@ namespace drogon::plugin
         {
             /// Initialize and start the plugin
             LOG_INFO << "DIPlugin  initialized and started";
+
             // Cấu hình injector ở đây
 
-            injector = di::make_injector(
-                di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique),  // UnitOfWork là singleton
-                di::bind<IPcPartService>.to<PcPartService>().in(di::unique),
-                di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::unique) // UserService nhận UnitOfWork
+            // injector = di::make_injector(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::shared), // UnitOfWork là singleton
+            //                              di::bind<IPcPartService>.to<PcPartService>().in(di::extension::shared)
+            //                              //  di::bind<IFatService>.to<FatService>().in(di::extension::shared),
+            //                              //  di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::shared)
+            // );
+
+            // injector = di::make_injector(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::session(_session)), // UnitOfWork là singleton
+            //                              di::bind<IPcPartService>.to<PcPartService>().in(di::extension::session(_session)),
+            //                              di::bind<IFatService>.to<FatService>().in(di::extension::session(_session)),
+            //                              di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::session(_session)) // UserService nhận UnitOfWork
+            // );
+
+            injector = di::make_injector(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::session(_session)), // UnitOfWork là singleton
+                                         di::bind<IPcPartService>.to<PcPartService>().in(di::extension::session(_session)),
+                                         di::bind<IFatService>.to<FatService>().in(di::extension::session(_session)),
+                                         di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::session(_session)) // UserService nhận UnitOfWork
             );
+
+            // injector = di::make_injector<di::extension::shared_config>(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::shared), // UnitOfWork là singleton
+            //                                                            di::bind<IPcPartService>.to<PcPartService>().in(di::extension::shared),
+            //                                                            di::bind<IFatService>.to<FatService>().in(di::extension::shared),
+            //                                                            di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::shared) // UserService nhận UnitOfWork
+            // );
 
             // injector.install(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique),  // UnitOfWork là singleton
             //                  di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::unique) // UserService nhận UnitOfWork)
@@ -24,7 +43,11 @@ namespace drogon::plugin
             // injector.install(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique), di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::unique));
             // injector.install(
             //     di::bind<Writer>().to<StdoutWriter>().in(di::unique), di::bind<Greeter>().to<GreeterImpl>().in(di::unique));
-            // injector = di::make_injector(di::bind<Writer>().to<StdoutWriter>().in(di::unique), di::bind<Greeter>().to<GreeterImpl>().in(di::unique));
+
+            // injectorTest = di::make_injector(di::bind<Writer>().to<StdoutWriter>().in(di::unique), di::bind<Greeter>().to<GreeterImpl>().in(di::unique));
+
+            // injectorTest = di::make_injector(di::bind<Writer>().to<StdoutWriter>().in(di::extension::shared), di::bind<Greeter>().to<GreeterImpl>().in(di::extension::shared));
+
             // injector = di::make_injector(di::bind<IWriterNew>().to<StdoutWriterNew>().in(di::unique), di::bind<IGreeterNew>().to<GreeterImplNew>().in(di::unique));
             // injector = di::make_injector(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique), di::bind<IGreeterNew>().to<GreeterImplNew>().in(di::unique));
         }
@@ -39,5 +62,71 @@ namespace drogon::plugin
         // Cleanup nếu cần
         /// Shutdown the plugin
         LOG_INFO << "DIPlugin shutdown";
+    }
+
+    bool DIPlugin::createSession(const std::string &sessionId)
+    {
+        // auto injector = std::make_unique<DIPlugin::InjectorType>(di::make_injector(di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::shared), // UnitOfWork là singleton
+        //                                                                                                                                                                      //  di::bind<IPcPartService>.to<PcPartService>().in(di::extension::shared),
+        //                                                                                                                                                                      //  di::bind<IFatService>.to<FatService>().in(di::extension::shared),
+        //                                                                            di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::shared)));
+        // sessions_[sessionId] = std::move(injector);
+        // std::cout << "Session created: " << sessionId << '\n';
+        // return true;
+
+        return true;
+    }
+    void DIPlugin::endSession(const std::string &sessionId)
+    {
+        // auto ptr = std::move(sessions_[sessionId]);
+        // ptr.reset();
+        // // auto injector = ptr.get();
+        // // LOG_INFO << "endSession ptr count: " << ptr.use_count();
+        // sessions_.erase(sessionId);
+        // // LOG_INFO << "endSession ptr count: " << ptr.use_count();
+
+        // std::cout << "Session ended for: " << sessionId << '\n';
+    }
+    // std::shared_ptr<di::extension::detail::session<drogon::AdviceChainCallback, di::scopes::singleton>> DIPlugin::createSession()
+    // {
+    //     auto ms = std::make_shared<di::extension::detail::session<drogon::AdviceChainCallback, di::scopes::singleton>>(di::extension::session(_session));
+    //     // Cấu hình injector ở đây
+    //     // injector = di::make_injector<di::extension::shared_config>(
+    //     //     di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::session(_session)), // UnitOfWork là singleton
+    //     //     di::bind<IPcPartService>.to<PcPartService>().in(di::extension::session(_session)),
+    //     //     di::bind<IFatService>.to<FatService>().in(di::extension::session(_session)),
+    //     //     di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::session(_session)) // UserService nhận UnitOfWork
+    //     // );
+    //     // injector = di::make_injector<di::extension::shared_config>(
+    //     //     di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::shared), // UnitOfWork là singleton
+    //     //     di::bind<IPcPartService>.to<PcPartService>().in(di::extension::shared),
+    //     //     di::bind<IFatService>.to<FatService>().in(di::extension::shared),
+    //     //     di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::shared) // UserService nhận UnitOfWork
+    //     // );
+    //     return ms;
+    // }
+
+    bool DIPlugin::createSessionEx(const std::string &sessionId)
+    {
+        // Tạo shared scope
+
+        // auto injector = std::make_shared<di::extension::injector<>>(di::make_injector(di::bind<Writer>().to<StdoutWriter>().in(di::extension::shared), di::bind<Greeter>().to<GreeterImpl>().in(di::extension::shared)));
+        // auto injector = std::make_shared<di::injector<std::shared_ptr<Writer>, std::shared_ptr<Greeter>>>(di::make_injector<di::extension::shared_config>(di::bind<Writer>().to<StdoutWriter>().in(di::extension::shared), di::bind<Greeter>().to<GreeterImpl>().in(di::extension::shared)));
+        DIPlugin::InjectorTypeTest injector = MakeAppInjector();
+        LOG_INFO << sizeof(sessionsEx_);
+        // sessionsEx_[sessionId] = std::move(injector);
+        this->sessionsEx_ = std::move(&injector);
+        // sessionsEx_.emplace(
+        //     sessionId,
+        //     injector);
+
+        // std::cout
+        //     << "Session created: " << sessionId << '\n';
+        return true;
+    }
+    void DIPlugin::endSessionEx(const std::string &sessionId)
+    {
+        // sessionsEx_.erase(sessionId);
+        std::cout << "Session ended for: " << sessionId << '\n';
     }
 }
--- plugins/DIPlugin.h
+++ plugins/DIPlugin.h
@@ -3,6 +3,9 @@
 #include <drogon/drogon.h>
 #include <drogon/plugins/Plugin.h>
 #include <boost/di.hpp>
+#include "boost/di/extension/scopes/shared.hpp"
+#include "boost/di/extension/scopes/session.hpp"
+#include "boost/di/extension/scopes/scoped.hpp"
 #include "boost/di/extension/injector.hpp"
 #include <iostream>
 #include "../repository/IUnitOfWork.hpp"
@@ -15,22 +18,39 @@
 
 #include "../services/IPcPartService.hpp"
 #include "../services/PcPartService.hpp"
+#include "../services/IFatService.hpp"
+#include "../services/FatService.hpp"
 
 #include "../repository/IWriterNew.hpp"
 #include "../repository/StdoutWriterNew.hpp"
 #include "../services/IGreeterNew.hpp"
 #include "../services/GreeterImplNew.hpp"
 
+#include <any>
+
 using namespace app_repositories;
 using namespace app_services;
 
 namespace di = boost::di;
+
+inline auto MakeAppInjector()
+{
+    return di::make_injector<di::extension::shared_config>(di::bind<Writer>().to<StdoutWriter>().in(di::extension::shared), di::bind<Greeter>().to<GreeterImpl>().in(di::extension::shared));
+}
 
 namespace drogon::plugin
 {
     class DIPlugin : public drogon::Plugin<DIPlugin>
     {
     public:
+        // using InjectorType = decltype(di::make_injector(
+        //     di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::singleton), // UnitOfWork là singleton
+        //     di::bind<IPcPartService>.to<PcPartService>().in(di::singleton),
+        //     di::bind<IFatService>.to<FatService>().in(di::singleton),
+        //     di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::singleton) // UserService nhận UnitOfWork
+        //     ));
+        // using InjectorType = di::extension::injector<>;
+
         DIPlugin() = default;
 
         /// This method must be called by drogon to initialize and start the plugin.
@@ -41,9 +61,16 @@ namespace drogon::plugin
         /// It must be implemented by the user.
         void shutdown() override;
 
+        bool createSessionEx(const std::string &sessionId);
+        void endSessionEx(const std::string &sessionId);
+
+        bool createSession(const std::string &sessionId);
+        void endSession(const std::string &sessionId);
+
         template <typename T>
         std::shared_ptr<T> get()
         {
+            // auto ms = di::extension::session(_session)();
             return injector.create<std::shared_ptr<T>>();
             // return injector.create<T>();
         }
@@ -55,19 +82,88 @@ namespace drogon::plugin
             // return injector.create<T>();
         }
 
+        // template <typename T>
+        // std::shared_ptr<T> getTest()
+        // {
+        //     auto ms = di::extension::session(_session)();
+        //     return injectorTest.create<std::shared_ptr<T>>();
+        //     // return injector.create<T>();
+        // }
+
+        // template <typename T>
+        // T getUniqueTest()
+        // {
+        //     return injectorTest.create<T>();
+        //     // return injector.create<T>();
+        // }
+
+        // Hàm template để lấy đối tượng từ session
+        // template <typename T>
+        // std::shared_ptr<T> getObjectBySession(const std::string &sessionId)
+        // {
+        //     auto it = sessions_.find(sessionId);
+        //     if (it != sessions_.end())
+        //     {
+        //         return it->second->create<std::shared_ptr<T>>();
+        //     }
+        //     else
+        //     {
+        //         throw std::runtime_error("Session not found: " + sessionId);
+        //     }
+        // }
+
+        // Hàm template để lấy đối tượng từ session
+        // template <typename T>
+        // std::shared_ptr<T> getObjectBySession(const std::string &sessionId)
+        // {
+        //     auto it = sessionsEx_.find(sessionId);
+        //     if (it != sessionsEx_.end())
+        //     {
+        //         return it->second->create<std::shared_ptr<T>>();
+        //     }
+        //     else
+        //     {
+        //         throw std::runtime_error("Session not found: " + sessionId);
+        //     }
+        // }
+
     private:
+        std::function<void()> _session = [] {};
         using InjectorType = decltype(di::make_injector(
-            di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique),
-            di::bind<IPcPartService>.to<PcPartService>().in(di::unique),
-            di::bind<app_services::IUserMicroserviceService>.to<app_services::UserMicroserviceService>().in(di::unique)));
+            di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::session(_session)), // UnitOfWork là singleton
+            di::bind<IPcPartService>.to<PcPartService>().in(di::extension::session(_session)),
+            di::bind<IFatService>.to<FatService>().in(di::extension::session(_session)),
+            di::bind<IUserMicroserviceService>.to<UserMicroserviceService>().in(di::extension::session(_session)) // UserService nhận UnitOfWork
+            ));
+        // using InjectorTypeTest = decltype(di::make_injector(di::bind<Writer>().to<StdoutWriter>().in(di::extension::shared), di::bind<Greeter>().to<GreeterImpl>().in(di::extension::shared)));
+        // using InjectorTypeTest = di::extension::injector<>;
+
+        // std::map<std::string, std::unique_ptr<InjectorType>> sessions_ = {};
+        // std::map<std::string, std::shared_ptr<di::injector<std::shared_ptr<Writer>, std::shared_ptr<Greeter>>>> sessionsEx_ = {};
+        // std::shared_ptr<di::injector<std::shared_ptr<Writer>, std::shared_ptr<Greeter>>> sessionsEx_[4];
+        // const InjectorTypeTest *sessionsEx_;
+
+        using InjectorTypeTest = decltype(MakeAppInjector());
+        // InjectorTypeTest *sessionsEx_ = new InjectorTypeTest(MakeAppInjector());
+        std::any sessionsEx_;
+
+        // using InjectorType = decltype(di::make_injector<di::extension::shared_config>(
+        //     di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::extension::shared),
+        //     di::bind<IPcPartService>.to<PcPartService>().in(di::extension::shared),
+        //     di::bind<IFatService>.to<FatService>().in(di::extension::shared),
+        //     di::bind<app_services::IUserMicroserviceService>.to<app_services::UserMicroserviceService>().in(di::extension::shared)));
 
         // using InjectorType = decltype(di::make_injector(
         //     di::bind<IWriterNew>().to<StdoutWriterNew>().in(di::unique), di::bind<IGreeterNew>().to<GreeterImplNew>().in(di::unique)));
 
         // using InjectorType = decltype(di::make_injector(
         //     di::bind<unitofwork::IUnitOfWork>.to<unitofwork::UnitOfWork>().in(di::unique), di::bind<IGreeterNew>().to<GreeterImplNew>().in(di::unique)));
-
         InjectorType injector;
+
+        // di::injector<std::shared_ptr<unitofwork::UnitOfWork>, std::shared_ptr<IPcPartService>, std::shared_ptr<IFatService>, std::shared_ptr<app_services::IUserMicroserviceService>> injector;
         // di::extension::injector<> injector{};
+
+        // using InjectorTypeTest = di::extension::injector<>;
+        // InjectorTypeTest injectorTest;
     };
 }
