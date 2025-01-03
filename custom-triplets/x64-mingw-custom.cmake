99244529ac4d0c021557b6e7ef44e52198e60145
quochuy0703 <quochuy0703@gmail.com>
2025-01-03 21:46:04 +0700

    1. update cmake

--- custom-triplets/x64-mingw-custom.cmake
+++ custom-triplets/x64-mingw-custom.cmake
@@ -1,11 +1,27 @@
+# Detect OS
+if(UNIX)
+    set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+else()
+    set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
+endif()
+
+# Common settings
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_ENV_PASSTHROUGH PATH)
 set(VCPKG_BUILD_TYPE release)
 
-set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
-if(${PORT} MATCHES  "librdkafka")
-    set(VCPKG_LIBRARY_LINKAGE static) # This changed from static to dynamic
+# Library-specific settings
+if(${PORT} MATCHES "librdkafka")
+    if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        # Configure for Linux
+        set(VCPKG_LIBRARY_LINKAGE dynamic)
+    else()
+        # Configure for MinGW
+        set(VCPKG_LIBRARY_LINKAGE static)
+    endif()
 endif()
+
+# Handle policies
 set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
